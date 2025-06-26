d228e21544e7007ff217c245833ab53438120539
Ambre-guia <ambreguia77@gmail.com>
2025-06-26 15:30:21 +0200

    refactor(index.php): simplify news section with template part
    Replace hardcoded news section with reusable template part to improve maintainability and reduce code duplication. The new implementation uses get_template_part() with configurable parameters for title, subtitle and post count.

--- index.php
+++ index.php
@@ -1,69 +1,20 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * The main template file
+ */
 
-<!-- NEWS SECTION -->
-<main class="wp-block-group has-black-background-color has-background is-layout-constrained wp-block-group-is-layout-constrained" style="padding-top:100px;padding-bottom:100px" id="wp--skip-link--target">
-    <h2 class="has-text-align-center zeever-animate zeever-move-up zeever-delay-1 has-white-color has-text-color has-heading-2-font-size wp-block-heading">Latest News</h2>
+get_header(); 
+?>
 
-
-
-    <h1 class="has-text-align-center zeever-animate zeever-move-up zeever-delay-3 has-zeever-secondary-color has-text-color has-tiny-font-size wp-block-heading" style="font-style:normal;font-weight:500;line-height:1.2;text-transform:capitalize;margin-right:0px;margin-bottom:60px">CHECK OUT SOME OF OUR NEWS</h1>
-
-
-    <?php if (have_posts()) : ?>
-        <div class="wp-block-query zeever-post-template-gap is-layout-flow wp-block-query-is-layout-flow">
-            <ul class="is-flex-container columns-3 zeever-animate zeever-move-up zeever-delay-5 wp-block-post-template is-layout-flow wp-block-post-template-is-layout-flow">
-                <?php while (have_posts()) : the_post(); ?>
-                    <li class="wp-block-post post-19 post type-post status-publish format-standard hentry category-uncategorized tag-boat tag-lake">
-
-
-                        <div style="font-size:14px;" class="wp-block-post-date has-text-color has-zeever-bodytext-color"><time datetime="2008-10-17T04:33:51+00:00"><?php the_date(); ?></time></div>
-
-                        <h2 style="font-style:normal;font-weight:500;" class="has-link-color wp-elements-b1fc05cfb458bcd456ac27e67599f438 wp-block-post-title has-large-font-size"><a href="<?php the_permalink(); ?>" target="_self"><?php the_title(); ?></a></h2>
-
-                        <div style="margin-top:20px;margin-bottom:20px;" class="has-link-color wp-elements-9b046888709cd4f53979f2daef11aa68 wp-block-post-excerpt has-text-color has-zeever-bodytext-color">
-                            <p class="wp-block-post-excerpt__excerpt"><?php the_excerpt(); ?></p>
-                            <p class="wp-block-post-excerpt__more-text"><a class="wp-block-post-excerpt__more-link" href="<?php the_permalink(); ?>">Read More</a></p>
-                        </div>
-                    </li>
-                <?php endwhile; ?>
-                <li class="wp-block-post post-19 post type-post status-publish format-standard hentry category-uncategorized tag-boat tag-lake">
-
-
-                    <div style="font-size:14px;" class="wp-block-post-date has-text-color has-zeever-bodytext-color"><time datetime="2008-10-17T04:33:51+00:00">October 17, 2008</time></div>
-
-                    <h2 style="font-style:normal;font-weight:500;" class="has-link-color wp-elements-b1fc05cfb458bcd456ac27e67599f438 wp-block-post-title has-large-font-size"><a href="https://wp-themes.com/zeever/?p=19" target="_self">Worth A Thousand Words</a></h2>
-
-                    <div style="margin-top:20px;margin-bottom:20px;" class="has-link-color wp-elements-9b046888709cd4f53979f2daef11aa68 wp-block-post-excerpt has-text-color has-zeever-bodytext-color">
-                        <p class="wp-block-post-excerpt__excerpt">Boat.</p>
-                        <p class="wp-block-post-excerpt__more-text"><a class="wp-block-post-excerpt__more-link" href="https://wp-themes.com/zeever/?p=19">Read More</a></p>
-                    </div>
-                </li>
-                <li class="wp-block-post post-36 post type-post status-publish format-standard hentry category-uncategorized">
-
-
-                    <div style="font-size:14px;" class="wp-block-post-date has-text-color has-zeever-bodytext-color"><time datetime="2008-09-05T19:39:12+00:00">September 5, 2008</time></div>
-
-                    <h2 style="font-style:normal;font-weight:500;" class="has-link-color wp-elements-b1fc05cfb458bcd456ac27e67599f438 wp-block-post-title has-large-font-size"><a href="https://wp-themes.com/zeever/?p=36" target="_self">Elements</a></h2>
-
-                    <div style="margin-top:20px;margin-bottom:20px;" class="has-link-color wp-elements-9b046888709cd4f53979f2daef11aa68 wp-block-post-excerpt has-text-color has-zeever-bodytext-color">
-                        <p class="wp-block-post-excerpt__excerpt">The purpose of this HTML is to help determine what default settings are with CSS and to make sure that all possible HTML Elements are included in this HTML so as to not miss any possible Elements when designing a site. Heading 1 Heading 2 Heading 3 Heading 4 Heading 5 Heading 6 [top] Paragraph…</p>
-                        <p class="wp-block-post-excerpt__more-text"><a class="wp-block-post-excerpt__more-link" href="https://wp-themes.com/zeever/?p=36">Read More</a></p>
-                    </div>
-                </li>
-                <li class="wp-block-post post-14 post type-post status-publish format-standard hentry category-uncategorized tag-wordpress">
-
-
-                    <div style="font-size:14px;" class="wp-block-post-date has-text-color has-zeever-bodytext-color"><time datetime="2008-06-21T00:09:24+00:00">June 21, 2008</time></div>
-
-                    <h2 style="font-style:normal;font-weight:500;" class="has-link-color wp-elements-b1fc05cfb458bcd456ac27e67599f438 wp-block-post-title has-large-font-size"><a href="https://wp-themes.com/zeever/?p=14" target="_self">More Tags</a></h2>
-
-                    <div style="margin-top:20px;margin-bottom:20px;" class="has-link-color wp-elements-9b046888709cd4f53979f2daef11aa68 wp-block-post-excerpt has-text-color has-zeever-bodytext-color">
-                        <p class="wp-block-post-excerpt__excerpt">More of these posts need tags.</p>
-                        <p class="wp-block-post-excerpt__more-text"><a class="wp-block-post-excerpt__more-link" href="https://wp-themes.com/zeever/?p=14">Read More</a></p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    <?php endif; ?>
+<main id="primary" class="site-main">
+    <?php
+    // Section de nouvelles
+    get_template_part('inc/template-part/block-news-section', null, array(
+        'title' => 'Latest News',
+        'subtitle' => 'CHECK OUT SOME OF OUR NEWS',
+        'posts_per_page' => 6
+    ));
+    ?>
 </main>
+
 <?php get_footer();
