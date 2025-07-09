7e66e435c45e96accef67944559d5eb19170faca
Ambre-guia <ambreguia77@gmail.com>
2025-07-09 16:22:56 +0200

    fix(tests): corriger l'indentation et améliorer la structure du code dans la section des témoignages

--- inc/template-part/block-testimonials-section.php
+++ inc/template-part/block-testimonials-section.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part pour la section de témoignages
  *
@@ -68,30 +69,30 @@ if (!isset($args['testimonials']) || empty($args['testimonials'])) {
         <div class="wp-block-column is-layout-flow">
             <div class="wp-block-group is-layout-flow">
                 <div class="wp-block-columns is-layout-flex">
-                    <?php 
+                    <?php
                     $delay = 1;
-                    foreach ($testimonials as $testimonial) : 
+                    foreach ($testimonials as $testimonial) :
                     ?>
-                    <div class="wp-block-column zeever-animate zeever-move-up zeever-delay-<?php echo $delay; ?> is-layout-flow">
-                        <div class="wp-block-group has-border-color has-zeever-border-border-color has-black-background-color has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;border-style:solid;border-width:1px;padding-top:40px;padding-right:40px;padding-bottom:80px;padding-left:40px">
-                            <div class="wp-block-columns is-layout-flex">
-                                <div class="wp-block-column is-layout-flow">
-                                    <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/quote.webp" alt="" class="wp-image-136" width="60"></figure>
+                        <div class="wp-block-column zeever-animate zeever-move-up zeever-delay-<?php echo $delay; ?> is-layout-flow">
+                            <div class="wp-block-group has-border-color has-zeever-border-border-color has-black-background-color has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;border-style:solid;border-width:1px;padding-top:40px;padding-right:40px;padding-bottom:80px;padding-left:40px">
+                                <div class="wp-block-columns is-layout-flex">
+                                    <div class="wp-block-column is-layout-flow">
+                                        <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo get_template_directory_uri(); ?>/asset/images/quote.webp" alt="" class="wp-image-136" width="60"></figure>
+                                    </div>
                                 </div>
+
+                                <p class="has-text-align-center has-zeever-bodytext-color has-text-color has-tiny-font-size"><em><?php echo esc_html($testimonial['quote']); ?></em></p>
                             </div>
 
-                            <p class="has-text-align-center has-zeever-bodytext-color has-text-color has-tiny-font-size"><em><?php echo esc_html($testimonial['quote']); ?></em></p>
+                            <figure class="wp-block-image aligncenter size-full is-resized zeever-margin-top-n40 is-style-rounded" style="border-radius:50px"><img src="<?php echo esc_url($testimonial['image']); ?>" alt="<?php echo esc_attr($testimonial['name']); ?>" width="70" height="70"></figure>
+
+                            <h2 class="has-text-align-center has-white-color has-text-color has-tiny-font-size" style="margin-top:15px"><?php echo esc_html($testimonial['name']); ?></h2>
+
+                            <h2 class="has-text-align-center has-zeever-bodytext-color has-text-color" style="font-size:14px;font-style:normal;font-weight:400;line-height:0"><?php echo esc_html($testimonial['position']); ?></h2>
                         </div>
-
-                        <figure class="wp-block-image aligncenter size-full is-resized zeever-margin-top-n40 is-style-rounded" style="border-radius:50px"><img src="<?php echo esc_url($testimonial['image']); ?>" alt="<?php echo esc_attr($testimonial['name']); ?>" width="70" height="70"></figure>
-
-                        <h2 class="has-text-align-center has-white-color has-text-color has-tiny-font-size" style="margin-top:15px"><?php echo esc_html($testimonial['name']); ?></h2>
-
-                        <h2 class="has-text-align-center has-zeever-bodytext-color has-text-color" style="font-size:14px;font-style:normal;font-weight:400;line-height:0"><?php echo esc_html($testimonial['position']); ?></h2>
-                    </div>
-                    <?php 
-                    $delay += 2;
-                    endforeach; 
+                    <?php
+                        $delay += 2;
+                    endforeach;
                     ?>
                 </div>
             </div>
