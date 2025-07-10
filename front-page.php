30207ac7425677797d68973b0a7e34ed266b8115
Ambre-guia <ambreguia77@gmail.com>
2025-07-10 14:55:18 +0200

    fix(front-page): réorganiser la section héros et améliorer la lisibilité du code

--- front-page.php
+++ front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The front page template file
  */
@@ -7,29 +8,33 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
+    <!-- Hero section -->
     <?php
-    // Hero section
-    get_template_part('inc/template-part/hero-frontpage');
-    
+    require_once get_theme_file_path('assets/templates-parts/hero-frontpage.php');
+    ?>
+
+
+    <?php
+
     // Services section
     get_template_part('inc/template-part/block-services-section', null, array(
         'title' => get_theme_mod('services_title', 'Services'),
         'subtitle' => get_theme_mod('services_subtitle', 'OUR SERVICES FOR CLIENTS')
     ));
-    
+
     // Projects section
     get_template_part('inc/template-part/block-projects-section', null, array(
         'title' => get_theme_mod('projects_title', 'Works'),
         'subtitle' => get_theme_mod('projects_subtitle', 'Projects We Have Done'),
         'posts_per_page' => 4
     ));
-    
+
     // Testimonials section
     get_template_part('inc/template-part/block-testimonials-section', null, array(
         'title' => get_theme_mod('testimonials_title', 'Testimonials.'),
         'subtitle' => get_theme_mod('testimonials_subtitle', 'FEEDBACK FROM OUR CLIENTS')
     ));
-    
+
     // News section
     get_template_part('inc/template-part/block-news-section', null, array(
         'title' => get_theme_mod('news_title', 'Latest News'),
