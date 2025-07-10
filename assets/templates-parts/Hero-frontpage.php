a547348999296cbe4b55be24b3f7d0c87a2ba6c1
Ambre-guia <ambreguia77@gmail.com>
2025-07-10 12:10:44 +0200

    feat(hero): ajout de la section hero avec un slider test

---
+++ assets/templates-parts/Hero-frontpage.php
@@ -0,0 +1,34 @@
+<section class="hero-frontpage">
+    <div id="slider">
+
+        <div class="slider-inner">
+            <div id="slider-content">
+                <div class="meta">Species</div>
+                <h2 id="slide-title">Amur <br>Leopard</h2>
+                <span data-slide-title="0">Amur <br>Leopard</span>
+                <span data-slide-title="1">Asiatic <br>Lion</span>
+                <span data-slide-title="2">Siberian <br>Tiger</span>
+                <span data-slide-title="3">Brown <br>Bear</span>
+                <div class="meta">Status</div>
+                <div id="slide-status">Critically Endangered</div>
+                <span data-slide-status="0">Critically Endangered</span>
+                <span data-slide-status="1">Endangered</span>
+                <span data-slide-status="2">Endangered</span>
+                <span data-slide-status="3">Least Concern</span>
+            </div>
+        </div>
+
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/leopard2.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/lion2.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg" />
+
+        <div id="pagination">
+            <button class="active" data-slide="0"></button>
+            <button data-slide="1"></button>
+            <button data-slide="2"></button>
+            <button data-slide="3"></button>
+        </div>
+
+    </div>
+</section>
