52cb8837888ac77aafead597919d6b27bce8defa
Ambre-guia <ambreguia77@gmail.com>
2025-07-10 15:19:40 +0200

    fix(config): ajouter des scripts manquants pour améliorer les fonctionnalités

--- inc/scripts/config.php
+++ inc/scripts/config.php
@@ -4,6 +4,9 @@ function my_load_scripts()
 {
 	wp_enqueue_script('fa', '//kit.fontawesome.com/055cc960f1.js');
 	wp_enqueue_script('aos', '//unpkg.com/aos@2.3.1/dist/aos.js');
+	wp_enqueue_script('tree', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r83/three.js');
+	wp_enqueue_script('image-loaded', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/4.1.4/imagesloaded.pkgd.js');
+	wp_enqueue_script('tween-max', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js');
 	wp_enqueue_script('tiny-slider', '//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js');
 	wp_enqueue_script('countdown', get_template_directory_uri() . '/assets/vendor/jquery.countdown.min.js', array('jquery'));
 	wp_enqueue_script('lightgallery', get_template_directory_uri()  . '/assets/vendor/lightgallery/lightgallery.min.js');
