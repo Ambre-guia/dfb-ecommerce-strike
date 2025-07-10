a1517d1c778df27034b94b76a6656aea74c5175a
Ambre-guia <ambreguia77@gmail.com>
2025-07-10 14:53:13 +0200

    fix(hero): améliorer la structure et les classes CSS du composant hero-slider

--- assets/templates-parts/Hero-frontpage.php
+++ assets/templates-parts/Hero-frontpage.php
@@ -1,16 +1,16 @@
 <section class="hero-frontpage">
-    <div id="slider">
+    <div id="slider" class="hero-slider">
 
         <div class="slider-inner">
-            <div id="slider-content">
+            <div id="slider-content" class="slider-content">
                 <div class="meta">Species</div>
-                <h2 id="slide-title">Amur <br>Leopard</h2>
+                <h2 id="slide-title" class="slider-title">Amur <br>Leopard</h2>
                 <span data-slide-title="0">Amur <br>Leopard</span>
                 <span data-slide-title="1">Asiatic <br>Lion</span>
                 <span data-slide-title="2">Siberian <br>Tiger</span>
                 <span data-slide-title="3">Brown <br>Bear</span>
                 <div class="meta">Status</div>
-                <div id="slide-status">Critically Endangered</div>
+                <div id="slide-status" class="slider-status">Critically Endangered</div>
                 <span data-slide-status="0">Critically Endangered</span>
                 <span data-slide-status="1">Endangered</span>
                 <span data-slide-status="2">Endangered</span>
@@ -23,11 +23,11 @@
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg" />
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg" />
 
-        <div id="pagination">
-            <button class="active" data-slide="0"></button>
-            <button data-slide="1"></button>
-            <button data-slide="2"></button>
-            <button data-slide="3"></button>
+        <div id="pagination" class="slider-pagination">
+            <button class="btn-slider active" data-slide="0"></button>
+            <button class="btn-slider" data-slide="1"></button>
+            <button class="btn-slider" data-slide="2"></button>
+            <button class="btn-slider" data-slide="3"></button>
         </div>
 
     </div>
