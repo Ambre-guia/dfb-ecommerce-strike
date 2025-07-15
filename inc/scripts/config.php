3a1c11aec548d114bb105fb3dbaffe43ff33ee48
Ambre-guia <ambreguia77@gmail.com>
2025-07-15 10:18:29 +0200

    refactor(config): update script dependencies and loading order
    - Rename 'tree' to 'three-js' for clarity
    - Move 'tween-max' before 'webgl-slider' and add it as dependency
    - Ensure all scripts are loaded in footer where appropriate

--- inc/scripts/config.php
+++ inc/scripts/config.php
@@ -4,10 +4,10 @@ function my_load_scripts()
 {
 	wp_enqueue_script('fa', '//kit.fontawesome.com/055cc960f1.js');
 	wp_enqueue_script('aos', '//unpkg.com/aos@2.3.1/dist/aos.js');
-	wp_enqueue_script('tree', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r83/three.js', array(), null, true);
+	wp_enqueue_script('three-js', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r83/three.js', array(), null, true);
 	wp_enqueue_script('image-loaded', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/4.1.4/imagesloaded.pkgd.js', array('jquery'), null, true);
-	wp_enqueue_script('webgl-slider', get_template_directory_uri() . '/assets/js/webgl-slider.js', array('tree', 'imagesloaded'), '1.0', true);
-	wp_enqueue_script('tween-max', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js');
+	wp_enqueue_script('tween-max', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js', array(), null, true);
+	wp_enqueue_script('webgl-slider', get_template_directory_uri() . '/assets/js/webgl-slider.js', array('three-js', 'imagesloaded', 'tween-max'), '1.0', true);
 	wp_enqueue_script('tiny-slider', '//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js');
 	wp_enqueue_script('countdown', get_template_directory_uri() . '/assets/vendor/jquery.countdown.min.js', array('jquery'));
 	wp_enqueue_script('lightgallery', get_template_directory_uri()  . '/assets/vendor/lightgallery/lightgallery.min.js');
