44523a1b0fb4a747d0ddf7aab87e899e9b7a78f3
Ambre-guia <ambreguia77@gmail.com>
2025-07-15 10:03:30 +0200

    refactor(hero-frontpage): restructure slider layout and use dynamic title
    Move slider images to separate container and use WordPress title function
    Reorganize HTML structure for better maintainability

--- assets/templates-parts/hero-frontpage.php
+++ assets/templates-parts/hero-frontpage.php
@@ -1,34 +1,37 @@
-<section class="hero-frontpage nopadding">
+<section class="hero-frontpage nopadding ">
     <div id="slider" class="hero-slider">
 
         <div class="slider-inner">
             <div id="slider-content" class="slider-content">
-                <div class="meta">Species</div>
-                <h2 id="slide-title" class="slider-title">Amur <br>Leopard</h2>
-                <span data-slide-title="0">Amur <br>Leopard</span>
-                <span data-slide-title="1">Asiatic <br>Lion</span>
-                <span data-slide-title="2">Siberian <br>Tiger</span>
-                <span data-slide-title="3">Brown <br>Bear</span>
-                <div class="meta">Status</div>
-                <div id="slide-status" class="slider-status">Critically Endangered</div>
-                <span data-slide-status="0">Critically Endangered</span>
-                <span data-slide-status="1">Endangered</span>
-                <span data-slide-status="2">Endangered</span>
-                <span data-slide-status="3">Least Concern</span>
+                <div class="meta">Titre</div>
+                <h2 id="slide-title"><?php echo get_the_title(); ?></h2>
             </div>
+            <span data-slide-title="0">Amur <br>Leopard</span>
+            <span data-slide-title="1">Asiatic <br>Lion</span>
+            <span data-slide-title="2">Siberian <br>Tiger</span>
+            <span data-slide-title="3">Brown <br>Bear</span>
+            <div class="meta">Status</div>
+            <div id="slide-status" class="slider-status">Critically Endangered</div>
+            <span data-slide-status="0">Critically Endangered</span>
+            <span data-slide-status="1">Endangered</span>
+            <span data-slide-status="2">Endangered</span>
+            <span data-slide-status="3">Least Concern</span>
         </div>
-
+    </div>
+    <div class="slider-images">
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/leopard2.jpg" />
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/lion2.jpg" />
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg" />
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg" />
 
-        <div id="pagination" class="slider-pagination">
-            <button class="btn-slider active" data-slide="0"></button>
-            <button class="btn-slider" data-slide="1"></button>
-            <button class="btn-slider" data-slide="2"></button>
-            <button class="btn-slider" data-slide="3"></button>
-        </div>
+    </div>
 
+
+
+    <div id="pagination" class="slider-pagination">
+        <button class="btn-slider active" data-slide="0"></button>
+        <button class="btn-slider" data-slide="1"></button>
+        <button class="btn-slider" data-slide="2"></button>
+        <button class="btn-slider" data-slide="3"></button>
     </div>
 </section>
