99e49f280292ad008d2071be5872c581d79e87f0
Ambre-guia <ambreguia77@gmail.com>
2025-07-22 12:06:20 +0200

    refactor(hero-frontpage): reorganize slider content and image order
    Move slider content elements into a more logical grouping and adjust image placement for better structure. The changes improve maintainability without altering functionality.

--- assets/templates-parts/hero-frontpage.php
+++ assets/templates-parts/hero-frontpage.php
@@ -1,30 +1,27 @@
 <section class="hero-frontpage nopadding">
     <div id="slider">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/leopard2.jpg" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/lion2.jpg" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg" />
-        
         <div class="slider-inner">
             <div id="slider-content" class="slider-content">
                 <div class="meta">Titre</div>
                 <h2 id="slide-title">Amur <br>Leopard</h2>
-                <div class="meta">Status</div>
-                <div id="slide-status" class="slider-status">Critically Endangered</div>
-            </div>
-            
-            <div class="slide-data">
                 <span data-slide-title="0">Amur <br>Leopard</span>
                 <span data-slide-title="1">Asiatic <br>Lion</span>
                 <span data-slide-title="2">Siberian <br>Tiger</span>
                 <span data-slide-title="3">Brown <br>Bear</span>
+                <div class="meta">Status</div>
+                <div id="slide-status" class="slider-status">Critically Endangered</div>
                 <span data-slide-status="0">Critically Endangered</span>
                 <span data-slide-status="1">Endangered</span>
                 <span data-slide-status="2">Endangered</span>
                 <span data-slide-status="3">Least Concern</span>
             </div>
         </div>
-        
+
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/leopard2.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/lion2.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg" />
+
         <div id="pagination" class="slider-pagination">
             <button class="btn-slider active" data-slide="0"></button>
             <button class="btn-slider" data-slide="1"></button>
